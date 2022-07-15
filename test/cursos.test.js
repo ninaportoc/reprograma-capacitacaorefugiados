e2c41414e90b9ee4beae5e26caf87e7a49f54c83
Marina Porto <marinaporto334@gmail.com>
2022-07-15 10:40:58 -0300

    Adicionei a rota de testes e o cursos.test.js

---
+++ test/cursos.test.js
@@ -0,0 +1,69 @@
+const cursosModel = require("../src/models/cursosModel")
+
+describe("GET route test", () => {
+    const course = new cursosModel({
+        "title": "teste curso",
+        "description": "teste descrição",
+        "category": "testes",
+        "courseID": ""
+    });
+    it("Deve chamar o schema e retornar o nome do curso", () => {
+        expect(course.title).toBe("teste curso");
+    });
+    it("Deve chamar o schema e retornar a descrição do curso", () => {
+        expect(course.description).toBe("teste descrição");
+    });
+    it("Deve chamar o schema e retornar a categoria do curso", () => {
+        expect(course.category).toStrictEqual(["testes"]);
+    });
+    it("Deve chamar o schema e retornar o id do curso", () => {
+        expect(JSON.stringify(course.courseID).substring(1, (JSON.stringify(course.courseID)).length - 1)).toBe("");
+    });
+});
+
+describe("ADD route test", () => {
+    const course = new cursosModel({
+        "title": "teste curso",
+        "description": "teste descrição",
+        "category": "testes",
+        "courseID": ""
+    });
+    it("Deve salvar o novo curso no banco de dados", () => {
+        course.save().then((dados) => {
+            expect(dados.title).toBe("teste curso");
+        });
+
+    });
+})
+
+describe("UPDATE route test", () => {
+    it("Deve editar o título do curso e adicioná-lo no banco de dados", () => {
+        const course = new cursosModel({
+            "title": "teste curso",
+            "description": "teste descrição",
+            "category": "testes",
+            "courseID": ""
+        });
+        course.title = "novo curso teste"
+        course.save().then((dados) => {
+            expect(dados.title).toBe("novo curso teste")
+        });
+    });
+})
+
+describe("DELETE route test", () => {
+    it("Deve excluir o novo curso", () => {
+        const course = new cursosModel({
+            "title": "teste curso",
+            "description": "teste descrição",
+            "category": "testes",
+            "courseID": ""
+        });
+        course.save().then((dados) => {
+            course.delete().then((novosdados) => {
+                expect(dados.title).toBe(null);
+            })
+        });
+    });
+})
+
